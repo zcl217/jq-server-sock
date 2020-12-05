0f5dac892858e6ca0b450c4313fbb7fcbeb0901b
Zachary Lin <zcl217@nyu.edu>
2020-12-05 22:32:24 +0900

    Revert port changes

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ sock.on('connection', function (connection) {
 
 const server = http.createServer(); 
 sock.installHandlers(server, {prefix:'/test'});
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3333;
 console.log("Listening on port: " + port);
 server.listen(port, '0.0.0.0');
 
